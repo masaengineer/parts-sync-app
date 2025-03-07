fcf02b04c4eabc5122055346a26c2b750645c72b
masaengineer <masahiro.track.a.f@gmail.com>
2025-03-07 23:45:35 +0900

    update: console.logとiconifyを削除

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
 // Entry point for the build script in your package.json
-console.log("Loading application.js");
 
 import { Turbo } from '@hotwired/turbo-rails';
 Turbo.start();
@@ -7,11 +6,5 @@ Turbo.start();
 import { Application } from '@hotwired/stimulus';
 import { registerControllers } from './controllers';
 
-// Import and initialize iconify-icon
-import 'iconify-icon';
-
-console.log("Starting Stimulus application");
 const application = Application.start();
-console.log("Registering controllers");
 registerControllers(application);
-console.log("Controllers registered");
