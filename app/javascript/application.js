53c88ad866e2b9ae0be64cc689ae601e814551f6
masaengineer <masahiro.track.a.f@gmail.com>
2025-03-07 23:53:22 +0900

    update: 標準仕様の書き方に修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,3 @@
 // Entry point for the build script in your package.json
-
-import { Turbo } from '@hotwired/turbo-rails';
-Turbo.start();
-
-import { Application } from '@hotwired/stimulus';
-import { registerControllers } from './controllers';
-
-const application = Application.start();
-registerControllers(application);
+import "@hotwired/turbo-rails"
+import "./controllers"
