aabef9917e67db0a96af40c78e714150f6b63353
masaengineer <masahiro.track.a.f@gmail.com>
2025-03-14 19:46:20 +0900

    delete: フラッシュメッセージの自動消去機能を削除

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,26 +2,22 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from './application';
 
-import ChartController from "./chart_controller"
-application.register("chart", ChartController)
+import ChartController from './chart_controller';
+application.register('chart', ChartController);
 
-import FlashController from "./flash_controller"
-application.register("flash", FlashController)
+import FormSubmitController from './form_submit_controller';
+application.register('form-submit', FormSubmitController);
 
-import FormSubmitController from "./form_submit_controller"
-application.register("form-submit", FormSubmitController)
+import ModalController from './modal_controller';
+application.register('modal', ModalController);
 
-import ModalController from "./modal_controller"
-application.register("modal", ModalController)
+import PasswordFieldController from './password_field_controller';
+application.register('password-field', PasswordFieldController);
 
-import PasswordFieldController from "./password_field_controller"
-application.register("password-field", PasswordFieldController)
+import ThemeController from './theme_controller';
+application.register('theme', ThemeController);
 
-
-import ThemeController from "./theme_controller"
-application.register("theme", ThemeController)
-
-import TourController from "./tour_controller"
-application.register("tour", TourController)
+import TourController from './tour_controller';
+application.register('tour', TourController);
